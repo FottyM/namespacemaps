c3718ece19f4e13c3cad2126fa4776635991f54d
Fortunat Mutunda <fotimutunda@gmail.com>
2017-12-28 16:26:32 +0200

    a bit of form styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { createTask} from "../actions/taskAction";
+import '../css/Form.css'
 
 // import StandaloneSearchBox from 'react-google-maps/lib/components/places/StandaloneSearchBox'
 
@@ -15,19 +16,15 @@ class Form extends Component{
     return(
       <div className="">
         <h1></h1>
-          <form onSubmit={(e) => this.submitForm(e)}>
-              <label>
-                Location
-                  <input type="text"/>
-              </label>
-              <label >
-                Category
+          <form className="form" onSubmit={(e) => this.submitForm(e)}>
+              <label> Address</label>
+              <input type="text"/>
+              <label> Category</label>
                   <select>
                       <option value="pickup">pickup</option>
                       <option value="drop_off">drop_off</option>
                   </select>
-              </label>
-              <input type="submit" value="ADD TASK"/>
+              <input type="submit" className='btn' value="ADD TASK"/>
           </form>
       </div>
     )
