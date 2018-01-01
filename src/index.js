368ae3e528a0218fbaee1b305b9c71e474f3af5f
Fortunat Mutunda <fotimutunda@gmail.com>
2018-01-01 16:07:34 +0200

    added redux devtool window

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import taskReducer from './reducers/taskReducer';
 
 import registerServiceWorker from './registerServiceWorker';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   combineReducers({ taskReducer }),
   {},
-  applyMiddleware(thunk, logger)
+  composeEnhancers(applyMiddleware(thunk, logger))
 );
 
 ReactDOM.render(
